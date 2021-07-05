fa2069294374c828b733afbfea17c687b6e83906
Cayle Sharrock <CjS77@users.noreply.github.com>
2021-07-05 09:05:49 +0200

    Source coverage (#48)
    * Support stable rust
    
    Updated dependencies such that Rust stable 1.53 is now supported.
    The optimised avx_2 option will NOT rust on stable because there's
    still an unstable feature on subtle-ng. BUT this feature is actually
    for doc generation and has been removed from Rust. As soon as subtle-ng
    merges https://github.com/dalek-cryptography/subtle/pull/85, avx2 will
    probably be supported on stable as well.
    
    The rand dep update breaks the benchmarks, but these will be fixed in
    the next commit.
    
    * Update benchmarks and fmt
    
    * Update benchmark code to use new dependency APIs
    * Run rust-fmt
    
    * Use source coverage in tests
    
    Replace the grcov action with the more accurate source coverage approach
    as detailed in
    https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/instrument-coverage.html
    
    This PR also provides the `test_coverage.sh` script that generates a
    test coverage report lcoally (see the comments for setting up
    pre-requisites)
    
    * Exclude std lib from coverage

---
+++ test_coverage.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Prerequisites
+# 1. You need LLVM-COV tools:
+# $ rustup component add llvm-tools-preview
+# 2. and Rust wrappers for llvm-cov:
+# $ cargo install cargo-binutils
+# 3. The rust name demangler
+# $ cargo install rustfilt
+# 4. jq
+# 5. genhtml
+# $ sudo apt install lcov
+
+RUSTFLAGS="-Z instrument-coverage"
+LLVM_PROFILE_FILE="./cov_raw/tari_crypto-%m.profraw"
+
+get_binaries() {
+  files=$( RUSTFLAGS=$RUSTFLAGS cargo test --tests --no-run --message-format=json \
+              | jq -r "select(.profile.test == true) | .filenames[]" \
+              | grep -v dSYM - \
+        );
+  files=("${files[@]/#/-object }")
+}
+
+get_binaries
+echo $files
+
+RUSTFLAGS=$RUSTFLAGS LLVM_PROFILE_FILE=$LLVM_PROFILE_FILE cargo test --tests
+
+cargo profdata -- \
+  merge -sparse ./cov_raw/tari_crypto-*.profraw -o ./cov_raw/tari_crypto.profdata
+
+cargo cov -- \
+  export \
+    --Xdemangler=rustfilt \
+    --format=lcov \
+    --show-branch-summary \
+    --show-instantiation-summary \
+    --show-region-summary \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex="^/rustc" \
+    --instr-profile=cov_raw/tari_crypto.profdata \
+    $files \
+    > cov_raw/tari_crypto.lcov
+
+if [ -z ${SKIP_HTML+x} ]; then
+  genhtml -o coverage_report cov_raw/tari_crypto.lcov
+else
+  echo "Skipping html generation"
+fi
+# open coverage_report/src/index.html
