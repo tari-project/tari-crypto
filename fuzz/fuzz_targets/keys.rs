3710e60a7fcd32b780c2037f415bddbd05f17205
stringhandler <mikethetike@tari.com>
2023-08-27 11:06:39 +0200

    tests: add fuzzing of secret key

---
+++ fuzz/fuzz_targets/keys.rs
@@ -0,0 +1,24 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use tari_crypto::ristretto::RistrettoSecretKey;
+use tari_crypto::tari_utilities::ByteArray;
+
+fuzz_target!(|data: &[u8]| {
+
+    if data.len() < 32 {
+        return;
+    }
+    match RistrettoSecretKey::from_bytes(&data[0..32]) {
+        Ok(r) => {
+            // WIP: this should fail because we do a mod.
+            // We should implement from_bytes_canonical
+            assert_eq!(&r.to_vec(), &data[0..32]);
+        }
+        Err(e) => {
+            // This is unlikely to fail
+            todo!()
+        }
+    }
+
+});
