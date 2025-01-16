a74aebd5237c3e2f7c155f5bf732bb7f3e3e7943
SW van Heerden <swvheerden@gmail.com>
2025-01-16 17:17:00 +0200

    test

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,8 @@ pub mod signatures;
 #[allow(clippy::op_ref)]
 pub mod ristretto;
 
-mod compressed_commitment;
-mod compressed_key;
+pub mod compressed_commitment;
+pub mod compressed_key;
 pub mod errors;
 #[cfg(feature = "bulletproofs_plus")]
 pub mod extended_range_proof;
